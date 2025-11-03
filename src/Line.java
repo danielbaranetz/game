faf818d34ab1d6550b81aee17f87b6be55dd5cd1
danielbaranetz <dbaranetz@gmail.com>
2025-11-03 18:06:14 +0200

    ver 1

--- src/Line.java
+++ src/Line.java
@@ -1,9 +1,5 @@
 public class Line {
     // constructors
-    private double x1;
-    private double x2;
-    private double y1;
-    private double y2;
     private Point start;
     private Point end;
 
@@ -11,22 +7,23 @@ public class Line {
         this.start = start;
         this.end = end;
     }
+
     public Line(double x1, double y1, double x2, double y2) {
-        this.x1 = x1;
-        this.x2 = x2;
-        this.y1 = y1;
-        this.y2 = y2;
+        this.start = new Point(x1, y1);
+        this.end = new Point(x2, y2);
     }
 
     // Return the length of the line
     public double length() {
-        return Math.sqrt(((x1 - x2)*(x1 - x2)) + ((y1 - y2)*(y1 - y2)));
+        return start.distance(end);
 
     }
 
     // Returns the middle point of the line
     public Point middle() {
-        return new Point((x1 + x2) / 2, (y1 + y2) / 2);
+        double midX = start.getX() + end.getX() / 2;
+        double midY = start.getY() + end.getY() / 2;
+        return new Point (midX,midY);
     }
 
     // Returns the start point of the line
@@ -41,7 +38,7 @@ public class Line {
 
     // Returns true if the lines intersect, false otherwise
     public boolean isIntersecting(Line other) {
-
+        return
     }
 
     // Returns the intersection point if the lines intersect,
