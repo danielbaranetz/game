1f26620a208a8b7ef87f4917190c39c9fc696ba8
danielbaranetz <dbaranetz@gmail.com>
2025-11-19 18:11:07 +0200

    angles and fixed bug when ball stuck inside blocks

--- src/Block.java
+++ src/Block.java
@@ -18,17 +18,36 @@ public class Block implements Collidable, Sprite {
 
     @Override
     public Velocity hit(Point collisionPoint, Velocity currentVelocity) {
-        double newDx = currentVelocity.getDx();
-        double newDy = currentVelocity.getDy();
-        if (collisionPoint.getY() == rect.getUpperLeft().getY() ||
-                collisionPoint.getY() == rect.getUpperLeft().getY() + rect.getHeight()) {
-            newDy = -newDy;
+        // הגדרת משתני עזר
+        double x = collisionPoint.getX();
+        double y = collisionPoint.getY();
+        double rectX = this.rect.getUpperLeft().getX();
+        double rectY = this.rect.getUpperLeft().getY();
+        double width = this.rect.getWidth();
+        double height = this.rect.getHeight();
+
+        double dx = currentVelocity.getDx();
+        double dy = currentVelocity.getDy();
+
+        double epsilon = 0.0001;
+
+        if (Math.abs(x - rectX) < epsilon || Math.abs(x - (rectX + width)) < epsilon) {
+            if (dx > 0) {
+                dx = -Math.abs(dx);
+            } else {
+                dx = Math.abs(dx);
+            }
         }
-        if (collisionPoint.getX() == rect.getUpperLeft().getX() ||
-                collisionPoint.getX() == rect.getUpperLeft().getX() + rect.getWidth()) {
-            newDx = -newDx;
+
+        if (Math.abs(y - rectY) < epsilon || Math.abs(y - (rectY + height)) < epsilon) {
+            if (dy > 0) {
+                dy = -Math.abs(dy);
+            } else {
+                dy = Math.abs(dy);
+            }
         }
-        return new Velocity(newDx, newDy);
+
+        return new Velocity(dx, dy);
     }
     @Override
     public void drawOn(DrawSurface d) {
